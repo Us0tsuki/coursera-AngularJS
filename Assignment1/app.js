0d10e933ea24d4a08c69b2e3bc295dcbaaf92049
Jialiang Yu <alex_yu@Jialiangs-MBP.gru.net>
2018-01-15 20:04:32 -0500

    First commit.

---
+++ Assignment1/app.js
@@ -0,0 +1,33 @@
+(function() {
+'use strict'
+angular.module('LunchCheck', []).controller('LunchCheckController', LunchCheckController)
+LunchCheckController.$inject = ['$scope'];
+function LunchCheckController($scope) {
+  $scope.items = "";
+  $scope.message = "";
+  $scope.showMessage = function() {
+    var items = $scope.items;
+    var message = $scope.message;
+    var itemArray = items.split(',');
+    var count = itemArray.length;
+    for (var i = 0; i < itemArray.length; i ++) {
+      if (itemArray[i] == "" || itemArray[i] == " ") {
+        count --;
+      }
+    }
+    if (items == "") {
+      $scope.message = "Please enter data first";
+      $scope.borderColor  = {"border-color": "red"};
+      $scope.fontColor = {"color": "red"};
+    } else if(count <= 3) {
+      $scope.message = "Enjoy!";
+      $scope.borderColor  = {"border-color": "green"};
+      $scope.fontColor = {"color": "green"};
+    } else {
+      $scope.message = "Too much!";
+      $scope.borderColor  = {"border-color": "green"};
+      $scope.fontColor = {"color": "green"};
+    }
+  }
+}
+})();
