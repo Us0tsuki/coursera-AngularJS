61939025a30953624e1cb794188a42bf422ccd13
Jialiang Yu <alex_yu@Jialiangs-MacBook-Pro.local>
2018-01-18 15:14:11 -0500

    better the functionality that handles the empty input case

--- Assignment1/app.js
+++ Assignment1/app.js
@@ -9,11 +9,14 @@ function LunchCheckController($scope) {
     var itemArray = $scope.items.split(',');
     var count = itemArray.length;
     for (var i = 0; i < itemArray.length; i ++) {
-      if (itemArray[i] == "" || itemArray[i] == " ") {
+      //if (itemArray[i] == "" || itemArray[i] == " ") { // This doesn't take into consideration that the input is multiple(>=2) spaces('   ')
+      if(itemArray[i].match(/^\s*$/)){ //use regular expression to solve this problem
+      // another alternative:
+      // if(jQuery.trim(itemArray[i]).length == 0){ //If you can solve the problem within JS easily, it doesn't make sense to import other dictionaries
         count --;
       }
     }
-    if (items == "") {
+    if ($scope.items == "") {
       $scope.message = "Please enter data first";
       $scope.borderColor  = {"border-color": "red"};
       $scope.fontColor = {"color": "red"};
